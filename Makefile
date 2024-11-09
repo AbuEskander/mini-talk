b6c283f431c6917752677d16e4c96e75c43fb478
Basil Ismail <bismail@c2r1s5.42amman.com>
2024-11-09 08:28:47 +0300

    Modified the make File

--- Makefile
+++ Makefile
@@ -1,28 +1,37 @@
-
-CLIENT = client
-SERVER_DIR = server_dir
-CLIENT_DIR = client_dir
+SRC_DIR = src
+OBJ_DIR = build
+CC = cc
+CFLAGS = -Wall -Werror -Wextra -I./includes
+NAME = minitalk
+SRCS_server = server.c
+SRCS_client = client.c
 SERVER = server
-NAME= miniTalk
+CLIENT = client
+SRCSDIR_server=$(addprefix src/,$(SRCS_server))
+SRCSDIR_client = $(addprefix src/,$(SRCS_client))
+OBJS_server = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCSDIR_server))
+OBJS_client = $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCSDIR_client))
 LIBFT = libft
-LIBFTA = libft.a
 
 all: $(NAME)
 
-$(NAME):
+$(NAME): $(OBJS_server) $(OBJS_client)
 	@make -C $(LIBFT)
-	@cp $(LIBFT)/$(LIBFTA) $(SERVER_DIR)
-	@cp $(LIBFT)/$(LIBFTA) $(CLIENT_DIR)
-	@make -C $(SERVER_DIR)
-	@make -C $(CLIENT_DIR)
-	@cp $(SERVER_DIR)/$(SERVER) .
-	@cp $(CLIENT_DIR)/$(CLIENT) .
-clean:
-	@make -C $(SERVER_DIR) fclean
-	@make -C $(CLIENT_DIR) fclean
+	@cp $(LIBFT)/$(LIBFT).a .
+	$(CC) $(OBJS_client) $(CFLAGS) -L. -lft -o $(CLIENT)
+	$(CC) $(OBJS_server) $(CFLAGS) -L. -lft -o $(SERVER)
+	
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
+	@mkdir -p $(OBJ_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+clean: 
 	@make -C $(LIBFT) fclean
+	rm -f  $(OBJS_client) $(OBJS_server) $(LIBFT).a
+
 fclean: clean
-	rm -f $(CLIENT) $(SERVER)
+	rm -f $(SERVER) $(CLIENT)
+
 re: fclean all
 
 .PHONY: all clean fclean re
--- includes/server.h
+++ includes/server.h
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.h                                           :+:      :+:    :+:   */
+/*   server.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bismail <bismail@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/02 11:33:53 by bismail           #+#    #+#             */
-/*   Updated: 2024/11/02 12:28:16 by bismail          ###   ########.fr       */
+/*   Created: 2024/11/02 11:33:15 by bismail           #+#    #+#             */
+/*   Updated: 2024/11/02 12:28:09 by bismail          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLIENT_H
-# define CLIENT_H
-# include "../../libft/includes/libft.h"
+#ifndef SERVER_H
+# define SERVER_H
+# include "../libft/includes/libft.h"
 # include <signal.h>
 # include <stdlib.h>
 # include <unistd.h>
